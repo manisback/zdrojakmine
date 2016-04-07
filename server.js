093470541e4b5605652e641d781252431ca32dae
manisback <lickavojtech@seznam.cz>
2016-04-07 16:30:16 +0200

    start
    5.lekce v serialu node.js zdrojak, stažen devtool

---
+++ server.js
@@ -0,0 +1,7 @@
+var express  = require('express')
+  , resource = require('express-resource')
+  , app = express();
+ 
+
+app.resource('pages', require('./app/pages'));
+app.listen(process.env.PORT || 5000);
